895998dd4d3adb0bcfb82587b599bac981430547
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-28 14:33:42 +0500

    Changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,5 @@
+import ButtonGradient from "../src/assets/svg/ButtonGradient";
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
