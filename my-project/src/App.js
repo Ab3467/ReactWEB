9fb88a26889a149ba8a1518a6f70cfc4a99de7d2
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-28 15:27:58 +0500

    Changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,16 @@
 import ButtonGradient from "../src/assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  <>
+  return (
+    <h1 className="text-3xl font-bold underline"></h1>;
+
+  <div className=" pt-[4.75rem] lg:pt-[5.25rem] underline">
+   <Button className= 'mt-10' href="#login"/>
+  </div>
+  <ButtonGradient/>
+  )
+  </>
+
 }
