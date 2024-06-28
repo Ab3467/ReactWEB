e91ab909ce9e570c071ef94640bda73317f5d0f4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-28 15:30:44 +0500

    Changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -3,12 +3,12 @@ import Button from "./components/Button";
 
 export default function App() {
   <>
-    return (<h1 className="text-3xl font-bold underline"></h1>;
+    return <h1 className="text-3xl font-bold underline"></h1>;
     <div className=" pt-[4.75rem] lg:pt-[5.25rem] underline">
       <Button className="mt-10" href="#login">
         Button
       </Button>
     </div>
-    <ButtonGradient />)
+    <ButtonGradient />
   </>;
 }
