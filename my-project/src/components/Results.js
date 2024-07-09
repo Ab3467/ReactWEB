556f9fc9333a4da93121eba7ec7205f2151c7044
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-09 12:23:41 +0500

    Changes

--- my-project/src/components/Results.js
+++ my-project/src/components/Results.js
@@ -3,7 +3,7 @@ import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ input }) {
   const resultData = calculateInvestmentResults(input);
-
+  console.log(resultData);
   return (
     <table id="result">
       <thead>
