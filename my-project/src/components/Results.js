b0dc3f81b11da39f0eb388b8b8ded5bfe6f6c40d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-09 12:00:21 +0500

    Changes

--- my-project/src/components/Results.js
+++ my-project/src/components/Results.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import { calculateInvestmentResults } from '../util/investment'
+import React from "react";
+import { calculateInvestmentResults } from "../util/investment";
 
-export default function Results({input}) {
-
-    const resultData = calculateInvestmentResults(input);
-    console.log(resultData);
+export default function Results({ input }) {
+  const resultData = calculateInvestmentResults(input);
+  console.log(resultData);
   return (
-    <table id='result'>
-      <tr>
-        <th>Year</th>
-        <th>Investment Value</th>
-        <th>Interest (Year)</th>
-        <th>Total Interest</th>
-        <th>Invested Capital</th>
-      </tr>
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
     </table>
-  )
+  );
 }
