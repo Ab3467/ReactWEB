6c8081cbd7f01c158487d4ff1cfcf56c83991305
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-09 10:26:38 +0500

    Changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
-  <>
-    return <h1 className="text-3xl font-bold underline">Hi</h1>;
-    <div className=" pt-[4.75rem] lg:pt-[5.25rem] underline">
-      
-    </div>
-   
-  </>;
+  return <div className=""></div>;
 }
