620be910be57b840db99cd2174e9c6e4a99cc506
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-09 10:45:57 +0500

    Changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import "../src/index.css"
+import UserInput from "./components/UserInput";
 
 export default function App() {
   return (
     <div>
       <Header/>
+      <UserInput/>
     </div>
   )
 }
---
+++ my-project/src/components/UserInput.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function UserInput() {
+  return (
+    <section id='user-input'>
+      <div className="input-info">
+        <label>Initial Investment</label>
+        <input type="text" name="" id="" />
+        <label>Annual Investment</label>
+        <input type="text" name="" id="" />
+        <label>Expected Return</label>
+        <input type="text" name="" id="" />
+        <label>Duration</label>
+        <input type="text" name="" id="" />
+      </div>
+    </section>
+  )
+}
