6d445d9a24de99a3519dce823618d1786689d038
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-10 11:06:19 +0500

    Changes

---
+++ my-project/src/components/UserInput.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function UserInput() {
+  return (
+    <div id="user-input ">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" />
+          <label>Initial Investment</label>
+          <input type="number" />
+        </p>
+      </div>
+      <div className="input-groups">
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" />
+          <label>Initial Investment</label>
+          <input type="number" />
+        </p>
+      </div>
+    </div>
+  );
+}
