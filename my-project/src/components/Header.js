6bc32cf98823d58870aa3ee5cb64e3cc502528b9
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-10 10:59:18 +0500

    Changes

---
+++ my-project/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+
+export default function Header() {
+  return (
+    <header id='header'>
+      <img src={logo} alt="" />
+    </header>
+  )
+}
