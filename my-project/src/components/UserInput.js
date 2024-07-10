6bf257c1e6e755ae7f11940b908c93a8be95541e
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-10 11:15:27 +0500

    Changes

--- my-project/src/components/UserInput.js
+++ my-project/src/components/UserInput.js
@@ -2,23 +2,27 @@ import React from "react";
 
 export default function UserInput() {
   return (
-    <div id="user-input ">
-      <div className="input-group">
-        <p>
-          <label>Initial Investment</label>
-          <input type="number" />
-          <label>Initial Investment</label>
-          <input type="number" />
-        </p>
-      </div>
-      <div className="input-groups">
-        <p>
-          <label>Initial Investment</label>
-          <input type="number" />
-          <label>Initial Investment</label>
-          <input type="number" />
-        </p>
-      </div>
-    </div>
+      <section id="user-input">
+        <div className="input-group">
+          <p>
+            <label>Initial Investment</label>
+            <input type="number"/>
+          </p>
+          <p>
+            <label>Annual Investment</label>
+            <input type="number"/>
+          </p>
+        </div>
+        <div className="input-group">
+          <p>
+            <label>Expected Return</label>
+            <input type="number"/>
+          </p>
+          <p>
+            <label>Duration</label>
+            <input type="number"/>
+          </p>
+        </div>
+      </section>
   );
 }
