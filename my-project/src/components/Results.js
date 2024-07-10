b7aa875ef3f78911a4ed4b2929c4c2fbcb7a5cbe
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-10 11:50:54 +0500

    Changes

--- my-project/src/components/Results.js
+++ my-project/src/components/Results.js
@@ -1,31 +1,33 @@
-import React from 'react'
-import calculateInvestmenthesults from "../util/investment.js"
+import React from "react";
+import calculateInvestmenthesults,{formatter} from "../util/investment.js";
 
-export default function Results({input}) {
+export default function Results({ input }) {
   const Results = calculateInvestmenthesults(input);
   console.log(Results);
   return (
-    <table id='result'>
+    <table id="result">
       <thead>
         <tr>
-        <th>Year</th>
-        <th>Investment Value</th>
-        <th>Interest (Value)</th>
-        <th>Total Interest</th>
-        <th>Invested Capital</th>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Value)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
         </tr>
       </thead>
       <tbody>
-        {Results.map((data)=>{
-          <tr>
-          <td>{data.year}</td>
-          <td>{data.valueEndOfYear}</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          </tr>
+        {Results.map((data) => {
+          return (
+            <tr key={data.year}>
+              <td>{data.year}</td>
+              <td>{formatter.format(data.valueEndOfYear)}</td>
+              <td>{formatter.format(data.interest)}</td>
+              <td>{formatter.format(data.totalInterest)}</td>
+              <td>{formatter.format(data.investedCapital)}</td>
+            </tr>
+          );
         })}
       </tbody>
     </table>
-  )
+  );
 }
