b3edcd789f7b48b325ba653be6791d2826860b61
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-10 11:33:10 +0500

    Changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,30 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import "../src/index.css";
 import UserInput from "./components/UserInput";
 import Results from "./components/Results";
 
 export default function App() {
-  const [input,setInput] = useState({
+  const [input, setInput] = useState({
     InitialInvestment: 10000,
     AnnualInvestment: 300,
     ExpectedReturn: 6,
-    Duration: 10
+    Duration: 10,
   });
 
-  function HandleInput(InitialValue,newValue){
-    setInput((prevValue)=>{
-      return{
-      ...prevValue,
-      [InitialValue]: newValue
-  }})
+  function HandleInput(InitialValue, newValue) {
+    setInput((prevValue) => {
+      return {
+        ...prevValue,
+        [InitialValue]: newValue,
+      };
+    });
   }
   return (
     <div>
       <Header />
-      <UserInput input={input} HandleInput={HandleInput}/>
-      <Results/>
+      <UserInput input={input} HandleInput={HandleInput} />
+      <Results />
     </div>
   );
 }
-
--- my-project/src/components/UserInput.js
+++ my-project/src/components/UserInput.js
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
 
-export default function UserInput({HandleInput,input}) {
-
-
+export default function UserInput({ HandleInput, input }) {
   return (
-      <section id="user-input">
-        <div className="input-group">
-          <p>
-            <label>Initial Investment</label>
-            <input type="number" value={input.InitialInvestment} onChange={(e)=>HandleInput('InitialInvestment',e.target.value)}/>
-          </p>
-          <p>
-            <label>Annual Investment</label>
-            <input type="number" value={input.AnnualInvestment} onChange={(e)=>HandleInput('AnnualInvestment',e.target.value)}/>
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label>Expected Return</label>
-            <input type="number" value={input.ExpectedReturn} onChange={(e)=>HandleInput('ExpectedReturn',e.target.value)}/>
-          </p>
-          <p>
-            <label>Duration</label>
-            <input type="number" value={input.Duration} onChange={(e)=>HandleInput('Duration',e.target.value)}/>
-          </p>
-        </div>
-      </section>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            value={input.InitialInvestment}
+            onChange={(e) => HandleInput("InitialInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            value={input.AnnualInvestment}
+            onChange={(e) => HandleInput("AnnualInvestment", e.target.value)}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            value={input.ExpectedReturn}
+            onChange={(e) => HandleInput("ExpectedReturn", e.target.value)}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            value={input.Duration}
+            onChange={(e) => HandleInput("Duration", e.target.value)}
+          />
+        </p>
+      </div>
+    </section>
   );
 }
