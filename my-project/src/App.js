63d5859d4945c4b181f377602f71ba072faea43d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-10 14:45:54 +0500

    Changes

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -18,7 +18,7 @@ export default function App() {
     setInput((prevValue) => {
       return {
         ...prevValue,
-        [InitialValue]: +newValue,
+        [InitialValue]: +newValue
       };
     });
   }
